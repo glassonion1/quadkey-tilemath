480b51f1fbab96ae513c9ceee124aa659096c5a8
taisuke.fujita <glassonion999@gmail.com>
2024-02-28 21:56:52 +0900

    update

--- dist/types/bbox.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Calculates the tile quadkey strings that are within a bounding box at a specific zoom level.
- * @param bounds A bounding box defined as an array of numbers in the format of [west, south, east, north].
- * @param zoom Zoom level to calculate tiles for.
- * @returns A list of quadkey strings.
- */
-export declare const getQuadkeysInBoundingBox: (west: number, south: number, east: number, north: number, zoom: number) => string[];
-//# sourceMappingURL=bbox.d.ts.map
